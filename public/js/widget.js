52d090b6fe9a6f0ae6f0dadc3e59337632ae4674
Wang lei <leiwang@thoughtworks.com>
2013-07-18 08:20:02 +0800

    implement basic navigator & advertisement switch

--- public/js/widget.js
+++ public/js/widget.js
@@ -1,8 +1,21 @@
 $(function() {
 
-    $(".navItem").on('click', function(e){
-        $(this).parent().find('.active').removeClass('active');
+    function changeNav(e){
+        e.preventDefault();
+        $(".navItem.active").removeClass("active");
         $(this).addClass('active');
+    }
 
-    })
+    function changeAdvItem(e){
+        e.preventDefault();
+        $("#viewport div.advItem").hide();
+        var activeDivId = $(this).data("item");
+        activeDivId = "#" + activeDivId;
+        $(activeDivId).show();
+    }
+
+    $(".navItem").on('click', changeNav);
+    $(".navItem").on('click', changeAdvItem);
+    $("#nav li").first().click();
+
 });
